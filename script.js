33de565bab5802b9df59308e1870b7de9885df60
Michael <m.anestopoulos24@gmail.com>
2023-03-13 19:48:35 -0400

    add modal with button (non functional)

--- script.js
+++ script.js
@@ -74,6 +74,20 @@ function playRound(playerSelection){
     
 }
 
+// Modal Setup
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+
+btn.onclick = function(){
+    modal.style.display = "block";
+}
+
+window.onclick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+}
+
 
 
 
